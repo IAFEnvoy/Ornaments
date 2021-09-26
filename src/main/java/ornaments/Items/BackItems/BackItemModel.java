d1ab81a445e42aae0ea5fc6b31e7a89b00b14701
IAFEnvoy <tanksheng545@126.com>
2021-09-26 19:41:32 +0800

    背置工具可以旋转了

--- src/main/java/ornaments/Items/BackItems/BackItemModel.java
+++ src/main/java/ornaments/Items/BackItems/BackItemModel.java
@@ -4,15 +4,19 @@ import com.google.common.collect.ImmutableList;
 import net.minecraft.client.model.ModelPart;
 import net.minecraft.client.render.entity.model.AnimalModel;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.util.math.MathHelper;
 import ornaments.Config.Configs;
 
 public class BackItemModel<T extends LivingEntity> extends AnimalModel<T> {
   public final ModelPart item;
-  public State state = State.IDLE;
+  private float offsetx, offsety;
 
   public BackItemModel() {
     textureWidth = 32;
     textureHeight = 16;
+
+    offsetx = 0;
+    offsety = 8;
 
     item = new ModelPart(this);
     item.setPivot(0.0F, 0.0F, 0.0F);
@@ -42,12 +46,14 @@ public class BackItemModel<T extends LivingEntity> extends AnimalModel<T> {
     this.item.pivotX = -8F;
     this.item.pivotY = -1F;
     this.item.pivotZ = 1F;
-    this.item.pitch = (float) (Configs.BackTools.rotateAngle.getDoubleValue()/180.0F*3.1415926F);
+    this.item.pitch = (float) (Configs.BackTools.rotateAngle.getDoubleValue()) / 180.0F * 3.1415926F;
     this.item.roll = 0F;
     this.item.yaw = 1.57F;
-  }
 
-  public enum State {
-    IDLE, CROUCHING, FLYING
+    float angle = (float) (Configs.BackTools.rotateAngle.getDoubleValue() - 135.0F) / 180.0F * 3.1415926F;
+    float r = (float) (8 * MathHelper.SQUARE_ROOT_OF_TWO);
+    this.item.pivotX = offsetx + r * MathHelper.sin(angle);
+    this.item.pivotY = offsety + r * MathHelper.cos(angle);
+
   }
 }
